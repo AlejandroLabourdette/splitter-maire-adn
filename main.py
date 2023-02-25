88a8c974de74d71b473b0af12d90d2507d8ea9a2
AlejandroLabourdette <jal.labourdette@gmail.com>
2023-02-25 17:11:03 -0500

    analyze multiple enzymes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import argparse
 import sys
 
+INDENT = '    '
 
 def arg_parser() -> argparse.ArgumentParser:
     argParser = argparse.ArgumentParser()
     argParser.add_argument("-i", "--input", help="path to the file where is written the dna sequence")
-    argParser.add_argument("-s", "--sequence", help="path to the file where is written the subsequence to match in the dna sequence")
     argParser.add_argument("-o", "--output", help="path to the file where the output should be stored")
 
     return argParser
@@ -14,11 +14,34 @@ def get_files_paths(parser: argparse.ArgumentParser, argv: list[str]):
     args = parser.parse_args(argv)
     if args.input == None:
         args.input = 'dna.txt'
-    if args.sequence == None:
-        args.sequence = 'match.txt'
     if args.output == None:
         args.output = 'output.txt'
     return args
+
+def analyze_enzyme_in_dna(dna: str, seq: str, output_file):
+    seq_len = len(seq)
+    positions_found = get_positions_found(dna, seq)
+    subsequences = get_subsequences(dna, seq)
+
+    output_file.write(INDENT + "Se encontraró la secuencia " + str(len(positions_found)) + " veces.\n")
+    positions_str = ""
+    for i in positions_found:
+        positions_str += " (" + str(i) + "," + str(i+seq_len) + ") "    
+    output_file.write(INDENT*2 + "Posiciones en las que se encontró: {" + positions_str + "} \n\n")
+
+
+    output_file.write(INDENT + "La secuencia de ADN se dividió en " + str(len(positions_found)+1) + " subsecuencias.\n")
+    for i in range(len(subsequences)):
+        output_file.write(INDENT*2 + "Subsecuencia "+str(i+1)+":\n")
+        weight, bases_count = analize_subsequence(subsequences[i])
+        output_file.write(INDENT*3 + "Peso:  " + str(weight) + " g/mol \n")
+        total = INDENT + "Total:" + str(bases_count['total'])
+        a = INDENT + "A:" + str(bases_count['a'])
+        c = INDENT + "C:" + str(bases_count['c'])
+        g = INDENT + "G:" + str(bases_count['g'])
+        t = INDENT + "T:" + str(bases_count['t'])
+        output_file.write(INDENT*3 + "Conteo de bases:" + total + a + c + g + t + " \n")
+        output_file.write(INDENT*3 + "Representación:  " + subsequences[i] + "\n")
 
 def get_positions_found(dna: str, seq: str) -> list[int]:
     seq_len = len(seq)
@@ -59,33 +82,21 @@ def main(argv: list[str]):
     args = get_files_paths(parser, argv)
 
     dna = open(args.input, "r").read()
-    seq = open(args.sequence, "r").read()
-    seq_len = len(seq)
-
-    positions_found = get_positions_found(dna, seq)
-    subsequences = get_subsequences(dna, seq)
-
     output_file = open(args.output, "w")
-    output_file.write("Se encontraró la secuencia " + str(len(positions_found)) + " veces.\n")
+
+    enzyme_patterns = {
+        "EcoRI": "gaattc",
+        "BamHI": "ggatcc",
+        "HindIII": "aagctt",
+        "NotI": "gcggccgc",
+        # Añadir más patrones de restricción según sea necesario
+    }
+
+    for enzyme_name, pattern in enzyme_patterns.items():
+        output_file.write("Análisis de la enzima: "+ enzyme_name+". Patrón: " + pattern + "\n")
+        analyze_enzyme_in_dna(dna, pattern, output_file)
+        output_file.write("\n\n")
     
-    positions_str = ""
-    for i in positions_found:
-        positions_str += " (" + str(i) + "," + str(i+seq_len) + ") "    
-    output_file.write("    Posiciones: {" + positions_str + "} \n\n")
-
-
-    output_file.write("La secuencia de ADN se dividió en " + str(len(positions_found)+1) + " subsecuencias.\n")
-    for i in range(len(subsequences)):
-        output_file.write("    Subsecuencia "+str(i+1)+":\n")
-        weight, bases_count = analize_subsequence(subsequences[i])
-        output_file.write("        Peso:  " + str(weight) + " g/mol \n")
-        total = "   Total:" + str(bases_count['total'])
-        a = "   A:" + str(bases_count['a'])
-        c = "   C:" + str(bases_count['c'])
-        g = "   G:" + str(bases_count['g'])
-        t = "   T:" + str(bases_count['t'])
-        output_file.write("        Conteo de bases:" + total + a + c + g + t + " \n")
-        output_file.write("        Representación:  " + subsequences[i] + "\n")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
